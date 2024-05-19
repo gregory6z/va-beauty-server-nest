0d1b6c86f02c21cff031d68f5e5e938ba03d1bc4
gregory6z <gregoryrag@gmail.com>
2024-05-19 15:01:57 +0200

    :heavy_check_mark: fix : bug render

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 
-import { EnvService } from "./infra/env/env.service"
 import * as express from "express"
 import { ExpressAdapter } from "@nestjs/platform-express"
 
@@ -12,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server))
 
   // const configService = app.get(EnvService)
-  const port = process.env.PORT || 3333
+  const port = process.env.PORT || 10000
 
   await app.listen(Number(port))
 }
