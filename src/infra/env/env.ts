56e05232e08322d8fc78c229c25ea8d57b7a07e6
gregory6z <gregoryrag@gmail.com>
2024-05-19 11:36:10 +0200

    :heavy_check_mark: fix : bug deploy

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
-  PORT: z.coerce.number().optional().default(3333),
+  PORT: z.coerce.number().default(3333),
   JWT_PUBLIC_KEY: z.string(),
   JWT_PRIVATE_KEY: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
